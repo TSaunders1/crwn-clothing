1efcd7746fc4d1717a38554f256da42c4395cb15
Toby Saunders <toby.saunders@live.co.uk>
2021-06-01 14:50:01 +0100

    updating our app with a shop page, collection preview component and collection item component

--- src/Components/directory/directory.component.jsx
+++ src/Components/directory/directory.component.jsx
@@ -48,10 +48,9 @@ class Directory extends React.Component {
     const { sections } = this.state;
     return (
       <div className="directory-menu">
-        {sections.map(({
-          title, imageUrl, id, size,
-        }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {sections.map(({ id, ...otherSectionProps }) => (
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
